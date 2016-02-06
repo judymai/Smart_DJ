507c39e7fb64d886d2af75eecf05e86aa84b8af0
ToyaDR <victoria.rosuello@gmail.com>
2016-02-06 01:36:48 -0500

    models started

--- smart_dj/models.py
+++ smart_dj/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+	likes =  models.ManyToManyField()
+	dislikes =
+	
+	def addLike ():
+	   return 0
+	def addDislike ():
+	   return 0
+	def rmLike ():
+	   return 0
+	def rmDislike ():
+	   return 0
+	   
+	   
+class Room(models.Model):
+    host = 0
+
+class Songs(models.Model):
