5ec4c556bbd3429eb3cadb056016c64a422e95e9
Marcus Todd <matodd@andrew.cmu.edu>
2016-02-06 01:09:11 -0500

    Deletd old webapps things, fixed .gitignore

--- webapps/urls.py
+++ webapps/urls.py
@@ -1,4 +1,4 @@
-"""smart_dj URL Configuration
+"""webapps URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
--- webapps/wsgi.py
+++ webapps/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for smart_dj project.
+WSGI config for webapps project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smart_dj.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapps.settings")
 
 application = get_wsgi_application()
