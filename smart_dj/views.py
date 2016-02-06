17778e815836ca4800787b92cc90a00d9615f8bf
Marcus Todd <matodd@andrew.cmu.edu>
2016-02-06 03:59:26 -0500

    Added layout

--- smart_dj/views.py
+++ smart_dj/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'smart_dj/about.html', {})
+
+def layout(request):
+    return render(request, 'smart_dj/layout.html', {})
--- webapps/urls.py
+++ webapps/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', 'smart_dj.views.index', name='index'),
+    url(r'^layout/$', 'smart_dj.views.layout', name='layout')
 ]
