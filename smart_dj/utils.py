d74c623fb5514ee061f8a2a68b0a15402537f301
Marcus Todd <matodd@andrew.cmu.edu>
2016-02-07 16:55:56 -0500

    Added bound to artist hits method

--- smart_dj/utils.py
+++ smart_dj/utils.py
@@ -9,7 +9,7 @@ SPOTIFY_CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
 client_credentials = SpotifyClientCredentials(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials)
 
-def get_artist_hits(artist_name):
+def get_artist_hits(artist_name, length=5):
     results = sp.search(q=artist_name,limit=1,offset=0,type='artist')
     artist_uri = results['artists']['items'][0]['uri']
-    return [i['id'] for i in sp.artist_top_tracks(artist_uri)['tracks']]
+    return [i['id'] for i in sp.artist_top_tracks(artist_uri)['tracks']][:length]
